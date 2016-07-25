98632005151c902abaf0b17e0afa5b84aa6a99d4
Harilal <harilal@InApp-PC.(none)>
2016-07-25 11:19:05 +0530

    basic added

--- dev/modules/home/HomeCtrl.js
+++ dev/modules/home/HomeCtrl.js
@@ -1,9 +1,10 @@
-define([], function() {
-	function start(){
-		alert("home");
-	}
+define(['jquery', 'underscore', 'text!./templates/Home.html'], function($, _, tmpl) {
+    function start() {
+        var utmpl = _.template(tmpl);
+        $("#maincontent").html(utmpl());
+    }
 
-	return{
-		start : start
-	};
+    return {
+        start: start
+    };
 });
